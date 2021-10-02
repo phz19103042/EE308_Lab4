299c4084ac8f21d4f8c27e7f22bd3d97634e878c
phz19103042 <90459663+phz19103042@users.noreply.github.com>
2021-10-02 16:44:17 +0800

    Add files via upload
    This is the code for the start interface of the Bobing program.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 博饼游戏
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GAME g = new GAME();
+            g.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RULE r = new RULE();
+            r.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // Cursor = new Cursor("1.cur");
+        }
+    }
+}
