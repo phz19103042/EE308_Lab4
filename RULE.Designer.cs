f100115ad300bc413c60d453f3b04d1c24245428
phz19103042 <90459663+phz19103042@users.noreply.github.com>
2021-10-02 17:01:43 +0800

    Add files via upload
    This is the code related to the program rules interface.

---
+++ RULE.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace 博饼游戏
+{
+    partial class RULE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RULE
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::博饼游戏.Properties.Resources._18;
+            this.ClientSize = new System.Drawing.Size(582, 837);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(600, 884);
+            this.MinimumSize = new System.Drawing.Size(600, 884);
+            this.Name = "RULE";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "游戏规则";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
